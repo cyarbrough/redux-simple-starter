c04d3c4a0c28c4d42b00814a43ec5a479100aab6
C. Yarbrough <cryarbrough@gmail.com>
2017-09-25 16:38:01 -0600

    Intro to State

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
-  render() {
-    return <input onChange={this.onInputChange} />;
-  }
+  /**
+   * Constructor
+   * @param {*} props 
+   */
+  constructor(props) {
+    super(props);
 
+    this.state = { term: '' };
+  }
+  /**
+   * Render
+   */
+  render() {
+    return <input onChange={ this.onInputChange } />;
+  }
+  /**
+   * onInputChange
+   * @param {*} e 
+   */
   onInputChange(e) {
     console.log(e.target.value);
   }
