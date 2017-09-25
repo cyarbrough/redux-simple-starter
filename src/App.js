2c73ebe77020e804ccf6776ce4bf092e7c4f073d
C. Yarbrough <cryarbrough@gmail.com>
2017-09-25 16:07:12 -0600

    Component Structure

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import SearchBar from './components/SearchBar';
 //import logo from './logo.svg';
 import './App.css';
 
@@ -7,7 +8,9 @@ const API_KEY = process.env.REACT_APP_API_KEY;
 class App extends Component {
   render() {
     return (
-      <div>API {API_KEY}</div>
+      <div>
+        <SearchBar />
+      </div>
     );
   }
 }
