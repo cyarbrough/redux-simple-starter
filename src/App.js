0a59f1abe5a30a67df93c81c91c2ca37032a3fa1
C. Yarbrough <cryarbrough@gmail.com>
2017-09-26 12:28:40 -0600

    Building Lists with Map

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/SearchBar';
+import VideoList from './components/VideoList';
 import './App.css';
 
-// const API_KEY = process.env.REACT_APP_API_KEY;
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 class App extends Component {
+  /**
+   * Constructor
+   * @param {*} props 
+   */
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({ key: API_KEY, term: 'surfboards' }, (videos) => {
+      this.setState({ videos });
+    });
+  }
+  /**
+   * Render
+   */
   render() {
     return (
       <div>
         <SearchBar />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
